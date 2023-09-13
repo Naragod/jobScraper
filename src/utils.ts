8414a2a7586af5d067cba3db257a72c9df126289
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-13 01:51:03 -0400

    Remove unused attribute

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const flatten = (arr: any[], _ = undefined): any[] => {
+export const flatten = (arr: any[]): any[] => {
   return arr
     .reduce((prev, curr) => {
       return prev.concat(Array.isArray(curr) ? flatten(curr) : curr);
