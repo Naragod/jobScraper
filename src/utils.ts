25797f6950c77fbc876313716fa22e670d276315
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-15 01:03:07 -0400

    Add timeElapsed function. Useful utility for measuring time performance

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,17 @@ export const flatten = (arr: any[]): any[] => {
     }, [])
     .filter((item: any) => item != undefined && item != "");
 };
+
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const timeElapsed = async (func: Function, ...args: any[]): Promise<any> => {
+  const now = new Date().getTime();
+  const result = await func.apply(null, args);
+  const end = new Date().getTime();
+  const secondsElapsed = Math.round((end - now) / 1000);
+  const millisecondsElapsted = (end - now) % 1000;
+  console.log(`${func.name} took ${secondsElapsed}.${millisecondsElapsted} seconds to execute.`);
+  return { result, timeElapsed };
+};
