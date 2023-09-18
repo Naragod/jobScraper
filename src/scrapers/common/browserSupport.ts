69b6d8d6f9a1ae74c6d523d7ba6e4c4f61bb5133
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-17 23:02:50 -0400

    Reduce timeout time from 5 to 3 seconds.

--- src/scrapers/common/browserSupport.ts
+++ src/scrapers/common/browserSupport.ts
@@ -2,7 +2,7 @@ import { Page, chromium, Browser } from "playwright";
 
 let browser: Browser | null;
 export const getBrowserPage = async (options: any = {}): Promise<Page> => {
-  const timeout = options.timeout || 1000 * 5;
+  const timeout = options.timeout || 1000 * 3;
   // Launch the browser and open a new blank page
   browser = browser || (await chromium.launch(options));
   const context = await browser.newContext();
