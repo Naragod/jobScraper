f8367c9f6b8c8151efc76ebbd57bc52725e03810
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-25 18:27:49 -0400

    Export classifiers in base file.

--- src/scrapers/canadaJobBank/main.ts
+++ src/scrapers/canadaJobBank/main.ts
@@ -4,23 +4,30 @@ import { writeToFile } from "../../emailer/fileHandler";
 import { getJobInformation } from "./applicationHandler";
 import { getAllJobPageLinks } from "../../apiRequests/canadaJobBank/requestHandler";
 import { handleJobApplicationsInParallel } from "../common/executionSupport";
-import { ApplyToJobsFn } from "../common/interfaces";
+import { IJobInfo, scrapeJobsFn } from "../common/interfaces";
+import { classifyJobs } from "../../classifiers/main";
 
-export const applyToJobs: ApplyToJobsFn = async (seachParams: any, applicationLimit = 100) => {
+export const scrapeJobs: scrapeJobsFn = async (seachParams: any, applicationLimit = 100) => {
   let applicationPage = 0;
   let applicationsViewed = 0;
   const now = new Date().toISOString();
+  let jobsInformation: IJobInfo[] = [];
 
   while (applicationsViewed < applicationLimit) {
     applicationPage += 1;
     const jobLinks = await getAllJobPageLinks(seachParams);
-    const { result } = await timeElapsed(handleJobApplicationsInParallel, jobLinks, getJobInformation);
+    const { result } = await timeElapsed<IJobInfo>(handleJobApplicationsInParallel, jobLinks, getJobInformation);
+    jobsInformation = jobsInformation.concat(result);
     applicationsViewed += jobLinks.length;
     console.log("applicationsViewed:", applicationsViewed);
     writeToFile(`job_data_${applicationPage}_${now}.json`, JSON.stringify(result));
   }
+
   await closeBrowser();
+  return jobsInformation;
 };
+
+export const matchJobs = (jobs: IJobInfo[], skills: string[]) => classifyJobs(jobs, skills);
 
 // take a look at this job posting.
 // can increase the number of information captured.
--- src/scrapers/jobLever/main.ts
+++ src/scrapers/jobLever/main.ts
@@ -1,25 +1,28 @@
 import { timeElapsed } from "../../utils";
-import { ApplyToJobsFn } from "../common/interfaces";
+import { IJobInfo, scrapeJobsFn } from "../common/interfaces";
 import { writeToFile } from "../../emailer/fileHandler";
 import { closeBrowser } from "../common/browserSupport";
 import { getJobInformation } from "./applicationHandler";
 import { handleJobApplicationsInParallel } from "../common/executionSupport";
 import { getAllJobPageLinks } from "../../apiRequests/jobLever/requestHandler";
 
-export const applyToJobs: ApplyToJobsFn = async (searchParams: any, applicationLimit = 10) => {
+export const scrapeJobs: scrapeJobsFn = async (searchParams: any, applicationLimit = 10) => {
   let applicationPage = 0;
   let applicationsViewed = 0;
   const now = new Date().toISOString();
+  const jobsInformation: IJobInfo[] = [];
 
   while (applicationsViewed < applicationLimit) {
     applicationPage += 1;
     const jobLinks = await getAllJobPageLinks(searchParams);
-    const { result } = await timeElapsed(handleJobApplicationsInParallel, jobLinks, getJobInformation);
+    const { result } = await timeElapsed<IJobInfo>(handleJobApplicationsInParallel, jobLinks, getJobInformation);
+    jobsInformation.push(result);
     applicationsViewed += jobLinks.length;
     console.log("applicationsViewed:", applicationsViewed);
     writeToFile(`${searchParams.company}_${applicationPage}_${now}.json`, JSON.stringify(result));
   }
   await closeBrowser();
+  return jobsInformation;
 };
 
 // const page = await getBrowserPage({ headless: true });
