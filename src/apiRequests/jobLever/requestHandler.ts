0c52c4298eaf571772d44c71d5d3de38f43e158a
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-28 13:58:50 -0400

    Fix path dependencies. Fix search url

--- src/apiRequests/jobLever/requestHandler.ts
+++ src/apiRequests/jobLever/requestHandler.ts
@@ -1,14 +1,14 @@
 import { baseURL } from "../../config/jobLeverl.config.json";
-import { AllJobsLinksGetterFn } from "../../scrapers/common/interfaces";
+import { AllJobsLinksGetterFn, IJobSearchOptions } from "../../scrapers/common/interfaces";
 import { findElementsInNodeList, getNodeList } from "../../utils/htmlTraversal";
 
-export const getAllJobPageLinks: AllJobsLinksGetterFn = async (searchParams: any) => {
-  const { company, location, commitment, worplaceType } = searchParams;
-  let url = `${baseURL}/${company}`;
-  url = worplaceType != undefined ? `${url}?worplaceType=${worplaceType}` : url;
-  url = location != undefined ? `${url}?location=${encodeURIComponent(location)}` : url;
-  url = commitment != undefined ? `${url}?commitment=${encodeURIComponent(commitment)}` : url;
-  const jobEntries = await getNodeList(url, "[class*='posting-apply']");
+export const getAllJobPageLinks: AllJobsLinksGetterFn = async (searchParams: IJobSearchOptions) => {
+  const { searchTerm, location, commitment, workplaceType } = searchParams;
+  let url = `${baseURL}/${searchTerm}?`;
+  url = workplaceType != undefined ? `${url}&workplaceType=${workplaceType}` : url;
+  url = location != undefined ? `${url}&location=${encodeURIComponent(location)}` : url;
+  url = commitment != undefined ? `${url}&commitment=${encodeURIComponent(commitment)}` : url;
+  const jobEntries = await getNodeList(encodeURI(url), "[class*='posting-apply']");
 
   // return all links
   return findElementsInNodeList(jobEntries, "A").map((item: any) => item.href);
