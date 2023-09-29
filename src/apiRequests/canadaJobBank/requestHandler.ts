46c902466ecc734155263ec986536dfb4ccef00b
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-29 00:25:28 -0400

    Clean up apiRequests

--- src/apiRequests/canadaJobBank/requestHandler.ts
+++ src/apiRequests/canadaJobBank/requestHandler.ts
@@ -5,8 +5,14 @@ import { findElementsInNodeList, getNodeList } from "../../utils/htmlTraversal";
 export const getAllJobPageLinks: AllJobsLinksGetterFn = async (searchParams: IJobSearchOptions): Promise<string[]> => {
   const { searchTerm, location, age = 7, page = 1, sort } = searchParams;
   const composedUrl = `${baseURL}/jobsearch/jobsearch`;
-  const url = `${composedUrl}?searchstring=${searchTerm}&locationstring=${location}&page=${page}&sort=${sort}&fage=${age}`;
-  const jobEntries = await getNodeList(encodeURI(url), "[id^='article-']");
+  const formattedSearchTerm = searchTerm.split(" ").join("+")
+  let url =  `${composedUrl}?searchstring=${formattedSearchTerm}`
+  
+  url = age !== undefined ? `${url}&fage=${age}` : url;
+  url = sort !== undefined ? `${url}&sort=${sort}` : url;
+  url = page !== undefined ? `${url}&page=${page}` : url;
+  url = location !== undefined ? `${url}&locationstring=${location}` : url;
+  const jobEntries = await getNodeList(url, "[id^='article-']");
 
   // return all links
   return findElementsInNodeList(jobEntries, "A")
--- src/apiRequests/linkedIn/requestHandler.ts
+++ src/apiRequests/linkedIn/requestHandler.ts
@@ -9,6 +9,7 @@ import { getNodeList } from "../../utils/htmlTraversal";
 // pay: f_SB2=22
 // i.e.: https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=Canada&f_TPR=r604800&f_SB2=22&f_JT=F&f_WT=2%2C1&position=1&pageNum=0
 
+// LinkedIn is very flakky. They seem to implement thorttling by IP. Sometimes I get search results, sometimes not.
 export const getAllJobPageLinks: AllJobsLinksGetterFn = async (searchParams: IJobSearchOptions) => {
   const { searchTerm, location, commitment, workplaceType, pay, age = 7, page = 0 } = searchParams;
   let url = `${baseURL}?keywords=${searchTerm}&f_TPR=r${60 * 60 * 24 * age}&pageNum=${page}`;
