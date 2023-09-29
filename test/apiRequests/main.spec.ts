a9720296f17f027105517fcc4a2b64892ea3bd2b
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-29 00:00:43 -0400

    Add apiRequest tests

---
+++ test/apiRequests/main.spec.ts
@@ -0,0 +1,23 @@
+import { getAllJobPageLinks as  getAllJobPageLinksLinkedIn } from "../../src/apiRequests/linkedIn/requestHandler";
+import { getAllJobPageLinks as  getAllJobPageLinksJobLever } from "../../src/apiRequests/jobLever/requestHandler";
+import { getAllJobPageLinks as  getAllJobPageLinksCanadaJobBank } from "../../src/apiRequests/canadaJobBank/requestHandler";
+
+describe("getAllJobPageLinks", () => {
+  it("LinkedIn - Usage", async () => {
+    const searchParams = { searchTerm: "software developer" };
+    const links = await getAllJobPageLinksLinkedIn(searchParams);
+    expect(links.length).toEqual(25);
+  });
+
+  it("CanadaJobBank - Usage", async () => {
+    const searchParams = { searchTerm: "software developer" };
+    const links = await getAllJobPageLinksCanadaJobBank(searchParams);
+    expect(links.length).toEqual(25);
+  });
+
+  it("JobLever - Usage", async () => {
+    const searchParams = { searchTerm: "netlfix" };
+    const links = await getAllJobPageLinksJobLever(searchParams);
+    expect(links.length).toEqual(25);
+  });
+});
