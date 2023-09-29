45730599d0865279b70e4e3f4b11f72d7b5332db
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-09-29 16:34:20 -0400

    Save records to postgres db.

--- src/database/main.ts
+++ src/database/main.ts
@@ -1,5 +1,19 @@
+import { IJobInfo } from "../scrapers/common/interfaces";
+import { destructureObj } from "../utils/main";
 import { sql } from "./db";
+import hash from "object-hash";
 
-export const insertJobRequirements = async (jobRequirements:any) => {
-    
-}
+const formatToDBTableStructure = (job: IJobInfo) => {
+  const columns = ["id", "link", "title", "location", "company", "pay", "description", "commitment"];
+  const { result, leftOvers } = destructureObj(job, columns);
+  return { id: hash(result), ...result, extra: JSON.stringify(leftOvers) };
+};
+
+export const saveJobInfo = async (jobInfo: IJobInfo[]) => {
+  const jobs = jobInfo.map(formatToDBTableStructure);
+  await Promise.allSettled(
+    jobs.map(async (job) => {
+      await sql`INSERT INTO job_information ${sql(job)}`.catch(console.error);
+    })
+  );
+};
