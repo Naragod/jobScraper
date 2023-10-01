e4135bdc32f03d00fc939813fcc4d08f76156b30
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-01 19:18:16 -0400

    Make db connection dependent on current running environment.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv"
 import postgres from "postgres";
 
-export const sql = postgres({ host: "localhost", port: 6321, db: "postgres", username: "postgres" });
+dotenv.config({path: `.env.${process.env.ENVIRONMENT}`})
+export const sql = postgres(`postgres://postgres@${process.env.DB_HOST}:${process.env.DB_PORT}/postgres`);
--- src/database/main.ts
+++ src/database/main.ts
@@ -6,13 +6,18 @@ export const saveJobInfo = async (jobInfo: IJobInfo[], formatters: any) => {
 
   await Promise.allSettled(
     jobInfo.map(async (job) => {
-      const jobInfoStructure = formatToJobInfoTableStructure(job);
-      const jobReqStructure = formatToJobRequirementsStructure(job);
-      const job_information_id_raw: any = await sql`INSERT INTO job_information ${sql(jobInfoStructure)} returning id`;
-      const job_requirements_id_raw: any = await sql`INSERT INTO job_requirements ${sql(jobReqStructure)} returning id`;
-      const job_information_id = job_information_id_raw[0].id;
-      const job_requirements_id = job_requirements_id_raw[0].id;
-      await sql`INSERT INTO jobs ${sql({ job_information_id, job_requirements_id })}`;
-    }),
+      try {
+        const infoFormat = formatToJobInfoTableStructure(job);
+        const jobReqFormat = formatToJobRequirementsStructure(job);
+        const job_information_id_raw: any = await sql`INSERT INTO job_information ${sql(infoFormat)} returning id`;
+        const job_requirements_id_raw: any = await sql`INSERT INTO job_requirements ${sql(jobReqFormat)} returning id`;
+        const job_information_id = job_information_id_raw[0].id;
+        const job_requirements_id = job_requirements_id_raw[0].id;
+        await sql`INSERT INTO jobs ${sql({ job_information_id, job_requirements_id })}`;
+      } catch (err) {
+        console.error(err);
+        return;
+      }
+    })
   );
 };
