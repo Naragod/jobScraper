788c5ef740a9645f8c815ec229cc0ce4596f30c8
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-02 02:33:38 -0400

    Clean up code. Wrap it in transaction. Add to jobs table if both insertions into job_requirements and job_information tables succed

--- src/database/main.ts
+++ src/database/main.ts
@@ -1,23 +1,25 @@
 import { IJobInfo } from "../scrapers/common/interfaces";
 import { sql } from "./db";
 
-export const saveJobInfo = async (jobInfo: IJobInfo[], formatters: any) => {
+export const saveJobInfo = async (jobInfo: IJobInfo[], formatters: any, jobBoard: string) => {
   const { formatToJobInfoTableStructure, formatToJobRequirementsStructure } = formatters;
+  const promises = jobInfo.map((job) => {
+    try {
+      const infoFormat = formatToJobInfoTableStructure(job);
+      const jobReqFormat = formatToJobRequirementsStructure(job);
+      return sql.begin(async (sql) => {
+        const jobInfoRawId: any = await sql`INSERT INTO job_information ${sql(infoFormat)} returning id`;
+        const jobReqRawId: any = await sql`INSERT INTO job_requirements ${sql(jobReqFormat)} returning id`;
+        const job_information_id = jobInfoRawId[0].id;
+        const job_requirements_id = jobReqRawId[0].id;
+        await sql`INSERT INTO jobs ${sql({ job_board: jobBoard, job_information_id, job_requirements_id })}`;
+      });
+    } catch (err) {
+      console.log("Failed on job:", job);
+      console.error(err);
+      return;
+    }
+  });
 
-  await Promise.allSettled(
-    jobInfo.map(async (job) => {
-      try {
-        const infoFormat = formatToJobInfoTableStructure(job);
-        const jobReqFormat = formatToJobRequirementsStructure(job);
-        const job_information_id_raw: any = await sql`INSERT INTO job_information ${sql(infoFormat)} returning id`;
-        const job_requirements_id_raw: any = await sql`INSERT INTO job_requirements ${sql(jobReqFormat)} returning id`;
-        const job_information_id = job_information_id_raw[0].id;
-        const job_requirements_id = job_requirements_id_raw[0].id;
-        await sql`INSERT INTO jobs ${sql({ job_information_id, job_requirements_id })}`;
-      } catch (err) {
-        console.error(err);
-        return;
-      }
-    })
-  );
+  return await Promise.allSettled(promises);
 };
