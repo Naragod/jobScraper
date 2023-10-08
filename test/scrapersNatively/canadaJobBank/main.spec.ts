f93edc8813be3ca1e9aec807fb72ceae47cbc376
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-08 03:50:48 -0400

    Clean up tests. Add more cases, and improve structure

--- test/scrapersNatively/canadaJobBank/main.spec.ts
+++ test/scrapersNatively/canadaJobBank/main.spec.ts
@@ -1,23 +1,31 @@
-import { getApplicationBasicInfoNatively, getJobRequirementsNatively } from "../../../src/scrapers/canadaJobBank/nativeParser";
+import {
+  getApplicationBasicInfoNatively,
+  getJobRequirementsNatively,
+} from "../../../src/scrapers/canadaJobBank/nativeParser";
 import { getHTMLStringFromFile } from "../../../src/utils/io";
 import { getLinkedDOMNode } from "../../../src/utils/nativeHtmlTraversal";
 import { jobInformation, jobRequirements } from "./data/page_results.json";
 
 describe("Canada Job Bank Scraper", () => {
   describe("CanadaJobBank Native Parser", () => {
-    const link = "https://www.jobbank.gc.ca/jobsearch/jobposting/39271451;jsessionid=0EE207FB8A6459E3CE099D9B5E26A865.jobsearch74?source=searchresults";
+    const link =
+      "https://www.jobbank.gc.ca/jobsearch/jobposting/39271451;jsessionid=0EE207FB8A6459E3CE099D9B5E26A865.jobsearch74?source=searchresults";
     const htmlString = getHTMLStringFromFile("test/scrapersNatively/canadaJobBank/data/page.html");
 
-    it("getApplicationBasicInfoNatively - Usage", async () => {
-      const html = getLinkedDOMNode(htmlString);
-      const result = getApplicationBasicInfoNatively(html);
-      expect(result).toEqual(jobInformation);
+    describe("getApplicationBasicInfoNatively", () => {
+      it("usage", () => {
+        const html = getLinkedDOMNode(htmlString);
+        const result = getApplicationBasicInfoNatively(html);
+        expect(result).toEqual(jobInformation);
+      });
     });
 
-    it("getJobRequirementsNatively - Usage", async () => {
+    describe("getJobRequirementsNatively", () => {
+      it("usage", async () => {
         const html = getLinkedDOMNode(htmlString);
         const result = getJobRequirementsNatively(html);
-        expect(result).toEqual(jobRequirements)
+        expect(result).toEqual(jobRequirements);
       });
+    });
   });
 });
--- test/scrapersNatively/jobLever/main.spec.ts
+++ test/scrapersNatively/jobLever/main.spec.ts
@@ -1,25 +1,40 @@
-import { getApplicationBasicInfoNatively, getJobRequirementsNatively } from "../../../src/scrapers/jobLever/nativeParser";
+import {
+  getApplicationBasicInfoNatively,
+  getJobRequirementsNatively,
+} from "../../../src/scrapers/jobLever/nativeParser";
 import { getHTMLStringFromFile } from "../../../src/utils/io";
 import { getLinkedDOMNode } from "../../../src/utils/nativeHtmlTraversal";
-import { jobInformation, jobRequirements } from "./data/netflix_results.json";
+import { jobInformation, jobRequirements, withoutJobReq } from "./data/netflix_results.json";
 
 describe("JobLever Scraper", () => {
   describe("JobLever Native Parser", () => {
     const link = "https://jobs.lever.co/netflix/44912894-f070-4989-9f9f-92ffb2ed210a";
     const htmlString = getHTMLStringFromFile("test/scrapersNatively/jobLever/data/netflix_page.html");
 
-    it("getApplicationBasicInfoNatively - Usage", async () => {
-      const html = getLinkedDOMNode(htmlString);
-      const {description: resultDescription, ...resultBasicInfo} = getApplicationBasicInfoNatively(link, html);
-      const { description, ...basicJobInformation } = jobInformation
-      expect(resultBasicInfo).toEqual(basicJobInformation);
-      expect(resultDescription.length).toEqual(description.length);
+    describe("getApplicationBasicInfoNatively", () => {
+      it("usage", async () => {
+        const html = getLinkedDOMNode(htmlString);
+        const { description: resultDescription, ...resultBasicInfo } = getApplicationBasicInfoNatively(link, html);
+        const { description, ...basicJobInformation } = jobInformation;
+        expect(resultBasicInfo).toEqual(basicJobInformation);
+        expect(resultDescription.length).toEqual(description.length);
+      });
     });
 
-    it("getJobRequirementsNatively - Usage", async () => {
+    describe("getJobRequirementsNatively", () => {
+      it("usage", async () => {
         const html = getLinkedDOMNode(htmlString);
         const result = getJobRequirementsNatively(html);
-        expect(result).toEqual(jobRequirements)
+        expect(result).toEqual(jobRequirements);
       });
+
+      it("without posting requirements", () => {
+        // TODO: fix test
+        const enablePageString = getHTMLStringFromFile("test/scrapersNatively/jobLever/data/enable_page.html");
+        const html = getLinkedDOMNode(enablePageString);
+        const result = getJobRequirementsNatively(html);
+        expect(result).toEqual(withoutJobReq);
+      });
+    });
   });
 });
