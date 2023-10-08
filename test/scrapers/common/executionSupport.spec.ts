436b49d6755a9d2d151ae434a310bcd48bc48396
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-08 05:16:50 -0400

    Update expected results to return tasks as part of jobRequirements

---
+++ test/scrapers/common/executionSupport.spec.ts
@@ -0,0 +1,45 @@
+import * as browserSupport from "../../../src/scrapers/common/browserSupport";
+import { handleJobApplicationsInParallel } from "../../../src/scrapers/common/executionSupport";
+
+describe("Execution Support", () => {
+  describe("handlerJobApplicationsInParallel", () => {
+    const options: any = { concurrent: 1 };
+
+    it("usage", async () => {
+      jest.spyOn(browserSupport, "getBrowserPage").mockImplementation(() => Promise.resolve(<any>{ close: () => {} }));
+      const mockGetJobInformation = jest.fn().mockImplementation((link: string) => {
+        return { link, applicationInfo: {}, jobRequirements: {}, err: false };
+      });
+
+      const links = ["a", "b"];
+      const expectedResult = [
+        { link: "a", applicationInfo: {}, jobRequirements: {}, err: false },
+        { link: "b", applicationInfo: {}, jobRequirements: {}, err: false },
+      ];
+
+      const { result, jobsToRetry } = await handleJobApplicationsInParallel(links, mockGetJobInformation, options);
+      expect(result).toEqual(expectedResult);
+      expect(jobsToRetry).toEqual([]);
+    });
+
+    it("usage - Jobs to retry exist", async () => {
+      jest.spyOn(browserSupport, "getBrowserPage").mockImplementation(() => Promise.resolve(<any>{ close: () => {} }));
+      const mockGetJobInformation = jest
+        .fn()
+        .mockImplementationOnce((link: string) => {
+          return { link, applicationInfo: {}, jobRequirements: {}, err: false };
+        })
+        .mockImplementationOnce((link: string) => {
+          return { link, applicationInfo: {}, jobRequirements: {}, err: "some error" };
+        });
+
+      const links = ["a", "b"];
+      const expectedResult = [{ link: "a", applicationInfo: {}, jobRequirements: {}, err: false }];
+      const expectedJobsToRetry = [{ link: "b", applicationInfo: {}, jobRequirements: {}, err: "some error" }];
+
+      const { result, jobsToRetry } = await handleJobApplicationsInParallel(links, mockGetJobInformation, options);
+      expect(result).toEqual(expectedResult);
+      expect(jobsToRetry).toEqual(expectedJobsToRetry);
+    });
+  });
+});
