69aab6c47d72828cb450b387da7f6d6ba12f5aac
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-08 04:56:54 -0400

    Improve interface definition. Make it IJobInfo more specific

--- src/scrapers/common/interfaces.ts
+++ src/scrapers/common/interfaces.ts
@@ -1,6 +1,34 @@
 import { Page } from "playwright";
 
+export interface IJobRequirements {
+  tasks: string[];
+  education?: string;
+  experience?: string;
+  languages?: string[];
+  techExperience?: string[];
+}
+
+export interface IApplicationInfo {
+  title: string;
+  company: string;
+  location: string;
+  pay: string;
+  description?: string[];
+  commitment?: string;
+  workplaceType?: string;
+  jobProvider?: string;
+  department?: string;
+  extra?: any;
+}
+
 export interface IJobInfo {
+  link: string;
+  applicationInfo: IApplicationInfo;
+  jobRequirements: IJobRequirements;
+  err: any;
+}
+
+export interface IJobInfoModified {
   link: string;
   applicationInfo: any;
   jobRequirements: any;
@@ -27,7 +55,7 @@ export interface IJobSearchOptions {
 }
 
 export interface JobInfoGetterFn {
-  (link: string, page: Page): Promise<IJobInfo>;
+  (link: string, page: Page): Promise<IJobInfoModified>;
 }
 
 export interface JobInfoGetterNativelyFn {
