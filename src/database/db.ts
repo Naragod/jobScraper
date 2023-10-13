6998c0ccc726678fe208ced9f3789ff9734257c4
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-13 15:49:07 -0400

    Add clean database query. Testing is the primary use of this function.

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,4 +2,12 @@ import dotenv from "dotenv";
 import postgres from "postgres";
 
 dotenv.config({ path: `.env.${process.env.ENVIRONMENT}` });
-export const sql = postgres(`postgres://postgres@${process.env.DB_HOST}:${process.env.DB_PORT}/postgres`);
+const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
+export const sql = postgres(`postgres://postgres@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
+
+export const cleanDatabase = async () => {
+  await sql.begin(async () => {
+    await sql`DELETE FROM job_information`;
+    await sql`DELETE FROM job_requirements`;
+  });
+};
