d1e0b3578842171e8aa33a3f549148b787ff0a5e
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-13 15:48:09 -0400

    Clean up saveJob function

--- src/database/main.ts
+++ src/database/main.ts
@@ -3,23 +3,30 @@ import { sql } from "./db";
 
 export const saveJobInfo = async (jobInfo: IJobInfo[], formatters: any, jobBoard: string) => {
   const { formatToJobInfoTableStructure, formatToJobRequirementsStructure } = formatters;
-  const promises = jobInfo.map((job) => {
+  for (let job of jobInfo) {
     try {
       const infoFormat = formatToJobInfoTableStructure(job);
       const jobReqFormat = formatToJobRequirementsStructure(job);
-      return sql.begin(async (sql) => {
+      const result = await sql.begin(async (sql) => {
         const jobInfoRawId: any = await sql`INSERT INTO job_information ${sql(infoFormat)} returning id`;
         const jobReqRawId: any = await sql`INSERT INTO job_requirements ${sql(jobReqFormat)} returning id`;
         const job_information_id = jobInfoRawId[0].id;
         const job_requirements_id = jobReqRawId[0].id;
-        await sql`INSERT INTO jobs ${sql({ job_board: jobBoard, job_information_id, job_requirements_id })}`;
+        const insertedIds = await sql`INSERT INTO jobs ${sql({
+          job_board: jobBoard,
+          job_information_id,
+          job_requirements_id,
+        })} RETURNING id`;
+        return insertedIds.map((item) => item.id);
       });
+      return result
     } catch (err) {
-      console.log("Failed on job:", job);
-      console.error(err);
-      return;
+      console.log("Failed on job:", job, err);
+      throw err
     }
-  });
+  }
+};
 
-  return await Promise.allSettled(promises);
+export const getJobRecord = async (id: string) => {
+  return await sql`SELECT * FROM jobs WHERE id = ${id}`;
 };
