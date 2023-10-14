84f7a6031022b83ed71007067b84ddb6746f5e40
Jason Schneider <jasonmschneider@outlook.com>
2023-10-13 21:10:45 -0400

    Added insertion

--- scripts/create_job_embeddings.py
+++ scripts/create_job_embeddings.py
@@ -5,6 +5,23 @@ from transformers import BertModel, BertTokenizer, BertConfig, BertModel
 import torch
 
 
+def insert_db(query):
+    db_name = 'postgres'
+
+    user = 'postgres'
+
+    host = 'localhost'
+
+    port = '6321'
+
+    conn = psycopg2.connect(dbname=db_name, user=user, host=host, port=port)
+
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cursor.execute(query)
+
+    conn.commit()
+
 def query_db(query):
     db_name = 'postgres'
 
@@ -28,6 +45,7 @@ def query_db(query):
     return row_dict
 
 def get_embedding_from_list(input_text):
+    print(input_text)
     encoded_dict = tokenizer.encode_plus(
                         input_text,                      # Sentence to split into tokens
                         add_special_tokens = True, # Add special token '[CLS]' and '[SEP]'
@@ -55,20 +73,26 @@ jobs = query_db('''SELECT id, job_information_id, job_requirements_id
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained("bert-base-uncased")
 for job in jobs:
+    print('\n\n\n')
+    print(job)
+    job_information = query_db(f"SELECT title FROM job_information WHERE id = '{job['job_information_id']}' ")
+    title = job_information[0]['title'].lower().split(' ')
+    job_requirements = query_db(f"SELECT tasks FROM job_requirements WHERE id = '{job['job_requirements_id']}' ")
+    print(job_requirements)
+    tasks = list(map(lambda x: x.lower(), job_requirements[0]['tasks']))
+    print(tasks)
 
-    job_information = query_db(f"SELECT title FROM job_information WHERE id = '{row['job_information_id']}' ")
-    title = job_information[0]['title'].lower()
-    job_requirements = query_db(f'SELECT tasks FROM job_requirements WHERE id = '{row['job_requirements_id']} ")
-    tasks = job_requirements[0]['tasks'].lower()
-
-    input_text = [title]
-    title_vector = get_embedding_from_list(input_text)
-    requirements_vector = get_embedding_from_list([tasks])
-    total_vector = get_embedding_from_list([title, tasks])
+    input_text = title
+    title_vector = get_embedding_from_list(input_text).tolist()
+    if (len(tasks) > 0):
+        requirements_vector = get_embedding_from_list(tasks).tolist()
+    else:
+        requirements_vector = get_embedding_from_list(title).tolist()
+    #total_vector = get_embedding_from_list([title, tasks])
     #input_text.extend(req['description']) 
-    insert_query = f'''INSERT INTO job_embeddings(title_vector, requirements_vector, total_vector, jobs_id)
-                            VALUES({title_vector}, {requirements_vector}, {total_vector}, {job['id']})'''
+    insert_query = f'''INSERT INTO job_embeddings(title_vector, requirements_vector, jobs_id)
+                            VALUES('{title_vector}', '{requirements_vector}', {job['id']})'''
     
-    print(insert_query)
+    insert_db(insert_query)
 
     
