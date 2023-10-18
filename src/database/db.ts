32e7035a393e617bfc296fef1f96666bcf524012
Naragod <mateo.naranjo@mail.utoronto.ca>
2023-10-18 02:07:57 -0400

    Fix ssl bug when connecting postgres on dev environment.

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,10 +2,14 @@ import dotenv from "dotenv";
 import postgres from "postgres";
 
 dotenv.config({ path: `.env.${process.env.ENVIRONMENT}` });
-const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD = "", DB_SSL } = process.env;
-export const sql = postgres(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
-  ssl: <any>DB_SSL,
-});
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD = "" } = process.env;
+
+const getSQLConnection = () => {
+  if (process.env.ENVIRONMENT == "prod")
+    return postgres(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, { ssl: true });
+  return postgres(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
+};
+export const sql = getSQLConnection();
 
 export const cleanDatabase = async () => {
   await sql.begin(async () => {
